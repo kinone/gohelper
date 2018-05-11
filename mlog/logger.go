9d6e37f3f9eeffc2e27e0d3e115b0793d569556d
zhenhao <hit_zhenhao@163.com>
2018-05-11 11:08:05 +0800

    add some useful func to mlog

--- mlog/logger.go
+++ mlog/logger.go
@@ -85,36 +85,82 @@ func (l *Logger) Log(level int, msg string, context ...interface{}) {
 	l.Println(lp, msg, context)
 }
 
+func (l *Logger) Logf(level int, format string, context ...interface{}) {
+	lp, exist := levelPrefix[level]
+	if !exist { // 不存在的level
+		return
+	}
+
+	if l.level > level { // 不需要处理的level
+		return
+	}
+
+	context = append([]interface{}{lp}, context...)
+	l.Printf("%s " + format, context...)
+}
+
 func (l *Logger) Debug(msg string, context ...interface{}) {
 	l.Log(LDEBUG, msg, context...)
+}
+
+func (l *Logger) Debugf(format string, context ...interface{}) {
+	l.Logf(LDEBUG, format, context...)
 }
 
 func (l *Logger) Info(msg string, context ...interface{}) {
 	l.Log(LINFO, msg, context...)
 }
 
+func (l *Logger) Infof(format string, context ...interface{}) {
+	l.Logf(LINFO, format, context...)
+}
+
 func (l *Logger) Notice(msg string, context ...interface{}) {
 	l.Log(LNOTICE, msg, context...)
+}
+
+func (l *Logger) Noticef(format string, context ...interface{}) {
+	l.Logf(LNOTICE, format, context...)
 }
 
 func (l *Logger) Warning(msg string, context ...interface{}) {
 	l.Log(LWARNING, msg, context...)
 }
 
+func (l *Logger) Warningf(format string, context ...interface{}) {
+	l.Logf(LWARNING, format, context...)
+}
+
 func (l *Logger) Error(msg string, context ...interface{}) {
 	l.Log(LERROR, msg, context...)
+}
+
+func (l *Logger) Errorf(format string, context ...interface{}) {
+	l.Logf(LERROR, format, context...)
 }
 
 func (l *Logger) Critical(msg string, context ...interface{}) {
 	l.Log(LCRITICAL, msg, context...)
 }
 
+func (l *Logger) Criticalf(format string, context ...interface{}) {
+	l.Logf(LCRITICAL, format, context...)
+}
+
 func (l *Logger) Alert(msg string, context ...interface{}) {
 	l.Log(LALERT, msg, context...)
 }
 
+func (l *Logger) Alertf(format string, context ...interface{}) {
+	l.Logf(LALERT, format, context...)
+}
+
 func (l *Logger) Emergency(msg string, context ...interface{}) {
 	l.Log(LEMERGENCY, msg, context...)
+}
+
+func (l *Logger) Emergencyf(format string, context ...interface{}) {
+	l.Logf(LEMERGENCY, format, context...)
 }
 
 func (l *Logger) Reload() error {
